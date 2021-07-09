b84014bc50d37b47c038975a3665e321a0e79c5b
tianhongliang <tianhong_liang@163.com>
2021-07-09 20:31:29 +0800

    nice

---
+++ exercises/clippy/target/debug/deps/clippy2-1b2bf51e5fe7085d.d
@@ -0,0 +1,7 @@
+/home/doubi/learn/rust/rustlings/exercises/clippy/target/debug/deps/clippy2-1b2bf51e5fe7085d.rmeta: clippy2.rs
+
+/home/doubi/learn/rust/rustlings/exercises/clippy/target/debug/deps/clippy2-1b2bf51e5fe7085d.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
